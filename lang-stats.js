5d34043c37c6f90ee6b9b2d176f57c735d0e4fd8
Eric Hernandez <herneri@protonmail.com>
2024-11-28 00:06:44 -0600

    feat: Add language usage calculation

---
+++ lang-stats.js
@@ -0,0 +1,40 @@
+/*
+	Copyright 2024 Eric Hernandez
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+/* 
+	Pass data that was retrieved from the API
+	and calculate the percent of usage for the languages.
+
+	NOTE: The total amount that languages are used is
+	calculated as they are being retrieved from the API.
+*/
+function language_makeup(languages_count, total_count) {
+	var largest = -1;
+	var language_stats = {
+		"most_used": null
+	};
+
+	const keys = Object.keys(languages_count);
+	keys.forEach(function(key) {
+		language_stats[key] = languages_count[key] / total_count;
+		if (language_stats[key] > largest) {
+			language_stats["most_used"] = key;
+			largest = language_stats[key];
+		}
+	});
+
+	return language_stats;
+}
