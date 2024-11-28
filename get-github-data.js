4f3e1ca0ab95e5a5abde0ff0a04f0e850f199c3a
Eric Hernandez <herneri@protonmail.com>
2024-11-28 00:20:55 -0600

    feat: Fetch and display latest active repo and number of public repos

--- get-github-data.js
+++ get-github-data.js
@@ -1,11 +1,32 @@
 async function github_api_handling() {
 	const GITHUB_URL = "https://api.github.com/users/herneri";
 
-	const response = await fetch(GITHUB_URL);;
-	if (!response.ok) {
-		document.write("ERROR: Failed to gather data from GitHub");
+	const user_data = await fetch(GITHUB_URL);;
+	const event_data = await fetch(GITHUB_URL + "/events");;
+
+	if (!user_data.ok) {
+		document.write("ERROR: Failed to gather user data from GitHub <br/>");
+		return;
+	} else if (!event_data.ok) {
+		document.write("ERROR: Failed to gather event data from GitHub <br/>");
 		return;
 	}
 
-	return await response.json();
+	return [await user_data.json(), await event_data.json()];
 }
+
+async function write_github_data() {
+	const api_data = await github_api_handling();
+	var data_section = document.getElementById("github_content");
+
+	var public_repos = document.createElement("p");
+	var most_recent_repo = document.createElement("p");
+
+	public_repos.textContent = "Number of Public Repos: " + api_data[0]["public_repos"];
+	data_section.appendChild(public_repos);
+
+	most_recent_repo.textContent = "Most Recently Updated Repo: " + api_data[1][0]["repo"]["name"];
+	data_section.appendChild(most_recent_repo);
+}
+
+write_github_data();
