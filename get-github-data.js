67bb477dc700e3add8015171dc7cd7b85f59b038
Eric Hernandez <herneri@protonmail.com>
2024-11-29 23:19:23 -0600

    refactor: Get number of public repos with one less API call

--- get-github-data.js
+++ get-github-data.js
@@ -2,25 +2,22 @@ import {track_language_count} from "./lang-stats.js";
 
 async function github_api_handling() {
 	const GITHUB_URL = "https://api.github.com/users/herneri";
-	var user_data;
 	var event_data;
 	var repo_data;
 
 	try {
-		user_data = await fetch(GITHUB_URL);
 		event_data = await fetch(GITHUB_URL + "/events");
 		repo_data = await fetch(GITHUB_URL + "/repos");
 	} catch (NetworkError) {
 		return -100;
 	}
 
-	if (!user_data.ok) {
-		return -1;
-	} else if (!event_data.ok) {
+	if (!event_data.ok) {
 		return -2;
 	}
 
-	return [await user_data.json(), await event_data.json(), await repo_data.json()];
+	repo_data = await repo_data.json();
+	return [Object.keys(repo_data).length, await event_data.json(), repo_data];
 }
 
 async function gather_repo_language_data(repos_json) {
@@ -61,9 +58,6 @@ async function write_github_data() {
 	case -100:
 		error_message.textContent = "NETWORK ERROR: GitHub data is unavailable";
 		return;
-	case -1:
-		error_message.textContent = "ERROR: Failed to gather user data from GitHub";
-		return;
 	case -2:
 		error_message.textContent = "ERROR: Failed to gather event data from GitHub";
 		return;
@@ -72,7 +66,7 @@ async function write_github_data() {
 	var public_repos = document.createElement("p");
 	var most_recent_repo = document.createElement("p");
 
-	public_repos.textContent = "Number of Public Repos: " + api_data[0]["public_repos"];
+	public_repos.textContent = "Number of Public Repos: " + api_data[0];
 	data_section.appendChild(public_repos);
 
 	most_recent_repo.textContent = "Most Recently Updated Repo: " + api_data[1][0]["repo"]["name"];
